baa44f1fff3eb01c95e533fc7e052e4371f35d4c
MiguelMatos24 <miguelmatoss24@gmail.com>
2022-07-12 08:43:12 +0100

    final

--- components/category/index.jsx
+++ components/category/index.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import Link from "next/link";
 
 export default function Category(props) {
-
   const { id, news, title } = props;
 
   return (
     <div id={id} className="container-fluid">
       <div className="news album py-4">
         <h2 className="text-center mb-3">{title}</h2>
-        <div className="container">
-          <div className="row g-3 ">
-
         <div>
           <div className="row g-4">
-
             {news.map((n) => (
               <div
                 className="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-3"
